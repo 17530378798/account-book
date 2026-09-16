935ae0393f4f9c280c35e928f1dc1b76b01d4dd7
17530378798 <113123025+17530378798@users.noreply.github.com>
2026-09-16 18:09:58 +0800

    修复手机筛选金额和Safari放大

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "offline-ledger-v32";
+const CACHE_NAME = "offline-ledger-v33";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=32",
-  "./app.js?v=32",
-  "./manifest.webmanifest?v=32",
+  "./styles.css?v=33",
+  "./app.js?v=33",
+  "./manifest.webmanifest?v=33",
   "./icon-180.png",
   "./icon-192.png",
   "./icon-512.png"
